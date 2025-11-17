80c82ff56291434152e4cd6b5dd4f8906d4356a4
miko <105974108+Hayper229@users.noreply.github.com>
2025-11-18 02:28:15 +0300

    Update FTP_format_convertor.py

--- FTP_format_convertor.py
+++ FTP_format_convertor.py
@@ -1,7 +1,17 @@
 with open('FTP_results.txt', 'r') as f:
-     ftp_links = f.read().split()
-     for link in ftp_links:
-         parts = link.split("//")[1].split("@")
-         user_pass = parts[0].split(":")
-         host = parts[1]
-         print(f"{host}:{user_pass[0]}:{user_pass[1]}")
+    ftp_links = f.read().splitlines()
+    for link in ftp_links:
+        if "[FTP]:" in link:
+            parts = link.split("ftp://")[1].split("@")
+            user_pass = parts[0].split(":")
+            host = parts[1]
+            print(f"{host}:{user_pass[0]}:{user_pass[1]}")
+
+
+#with open('FTP_results.txt', 'r') as f:
+  #   ftp_links = f.read().split()
+   #  for link in ftp_links:
+    #     parts = link.split("//")[1].split("@")
+     #    user_pass = parts[0].split(":")
+      #   host = parts[1]
+       #  print(f"{host}:{user_pass[0]}:{user_pass[1]}")
